85498f21129255e59300525b787ce568909ea9de
dutraneto <dutraneto@me.com>
2022-02-14 07:07:07 -0500

    Add SidebarMenu component

---
+++ src/components/SidebarMenu/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'About',
+    url: '/'
+  },
+  {
+    label: 'Blog',
+    url: '/blog/'
+  },
+  {
+    label: 'Works',
+    url: '/works/'
+  },
+  {
+    label: 'Laboratory',
+    url: '/lab/'
+  }
+]
+
+export default links
