9437230fd811c5b0a7c4fd1ccc5c76315a6d496e
dutraneto <dutraneto@me.com>
2022-02-15 20:32:02 -0500

    Add MenuBar component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Profile from 'components/Profile'
 import Sidebar from 'components/Sidebar'
+import MenuBar from 'components/MenuBar'
 import * as S from './styles'
 
 import { userProfile } from '../../lib/static'
@@ -12,7 +13,7 @@ export default function Layout({ children }) {
 
   return (
     <S.LayoutWrapper>
-      <Profile author={author} position={position} isMobileHeader={true} />
+      <Profile author={author} position={position} isMobileHeader />
       <Sidebar
         author={author}
         position={position}
@@ -20,6 +21,7 @@ export default function Layout({ children }) {
         isMenuOpen={isMenuOpen}
       />
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
     </S.LayoutWrapper>
   )
 }
